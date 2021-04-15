6479ef376952cffe9d12b0d60b7bc5162cb3f5cc
Neil Chadwick <neilchadders@hotmail.com>
2021-04-15 08:43:42 +0100

    amnending html in message box

--- index.js
+++ index.js
@@ -5,9 +5,22 @@ const messageContainer = document.querySelector(".message-container");
 const message = document.getElementById("message");
 
 
+let isValid = false;
+let passwordsMatch = false;
+
+function validateForm(){
+	isValid = form.checkValidity();
+	console.log(isValid);
+}
 
 function processFormData(e){
-	console.log(e);
+	e.preventDefault();
+	
+	validateForm();
+
+	message.textContent = "Please fill out all fields.";
+	message.style.color = "red";
+	messageContainer.style.borderColor = "red";
 
 }
 
