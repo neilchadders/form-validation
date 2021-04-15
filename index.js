76a393462a8134cb694d55098e52ff3dfdb7b3fa
Neil Chadwick <neilchadders@hotmail.com>
2021-04-15 07:57:30 +0100

    adding event listener to JS

--- index.js
+++ index.js
@@ -4,51 +4,13 @@ const password2El = document.getElementById("password2");
 const messageContainer = document.querySelector(".message-container");
 const message = document.getElementById("message");
 
-let isValid = false;
-
-function validateForm(){
-	isValid = form.checkValidity();
-	//style main message for an error
-
-if (!isValid) {
-	message.textContent = "Please fill out all fields.";
-	message.style.color = "red";
-	messageContainer.style.borderColor = "red";
-	}
-
-	//Check to see if passwords match
-	if (password1El.value === password2El.value){
-
-		passwordsMatch = true;
-		password1El.style.borderColor = "green";
-		password2El.style.borderColor = "green";
-	} else {
-		passwordsMatch = false;
-		message.textContent = "Make sure passwords match.";
-		message.style.color = "red";
-		messageContainer.style.borderColor = "red";
-		password1El.style.borderColor = "red";
-		password2El.style.borderColor = "red";
-
-	}
-
-	if (isValid && passwordsMatch) {
-		message.textContent = "Successfully Registered";
-		message.style.color = "green";
-		messageContainer.style.borderColor = "green";
-	}
-}
 
 
 function processFormData(e){
-	e.preventDefault();
-
-	//Validate Form
-	validateForm();
-
+	console.log(e);
 
 }
 
-//Event Listener
+//Event Listener 
 
-form.addEventListener("submit", processFormData)
+form.addEventListener("submit", processFormData);
